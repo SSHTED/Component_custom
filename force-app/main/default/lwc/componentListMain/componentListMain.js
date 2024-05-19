b8026dabbe9a6b7f75fb0eb673534bf71ec135da
sshted <sshted5595@email.com>
2024-05-19 22:14:38 +0900

    feat: Integrate ComponentListMain with all subComponents

--- force-app/main/default/lwc/componentListMain/componentListMain.js
+++ force-app/main/default/lwc/componentListMain/componentListMain.js
@@ -1,20 +1,38 @@
+// componentListMain.js
 import { LightningElement } from 'lwc';
+import ComponentListHeader from 'c/componentListHeader';
+import ComponentListFilter from 'c/componentListFilter';
+import ComponentListTable from 'c/componentListTable';
+import ComponentListTile from 'c/componentListTile';
+import ComponentListModal from 'c/componentListModal';
+import ComponentListData from 'c/componentListData';
+import ComponentListAction from 'c/componentListAction';
 
-
-export default class ComponentList_main extends LightningElement {
-
-    renderedCallback() {
+export default class ComponentListMain extends LightningElement {
+    data = [];
+    columns = [];
+    
+    handleDataLoad() {
     }
 
-    handleFilterChange(event) {
+    handleFilterChange() {
     }
 
-    handleRowAction(event) {
+    handleRowSelect() {
     }
 
-    handleTileAction(event) {
+    handleRowAction() {
     }
 
-    handleModalAction(event) {
+    handleTileSelect() {
+    }
+
+    handleTileAction() {
+    }
+
+    handleModalAction() {
+    }
+    
+    handleAction(event) {
     }
 }
